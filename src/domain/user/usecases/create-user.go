86da1ec5b2fe16ab62d35ca0e3c746db68b171f8
Felipe Fadoni <felipefadonimt@gmail.com>
2022-08-22 22:24:02 -0300

    feat: adicionando hash password

--- src/domain/user/usecases/create-user.go
+++ src/domain/user/usecases/create-user.go
@@ -5,14 +5,28 @@ import (
 	"github.com/felipefadoni/boilerplate-golang/src/dto"
 	"github.com/felipefadoni/boilerplate-golang/src/helpers"
 	"github.com/felipefadoni/boilerplate-golang/src/infra/postgres/entities"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func CreateUserUseCase(userDTO dto.CreateUserDTO) (dto.CreateUserReturnDTO, error) {
-	userEntity := entities.User{}
+	var userEntity = entities.User{}
+
+	// Hashing the password with the default cost of 10
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDTO.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return dto.CreateUserReturnDTO{}, err
+	}
+
 	helpers.TransformData(userDTO, &userEntity)
+
+	userEntity.Permission = "[\"user::default\"]"
+	userEntity.Password = string(hashedPassword)
+
 	user, err := user.CreateUser(userEntity)
+
 	if err != nil {
 		return user, err
 	}
+
 	return user, nil
 }
--- src/infra/postgres/entities/user.go
+++ src/infra/postgres/entities/user.go
@@ -6,10 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	ID       string `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Name     string `gorm:"type:varchar(200);not null"`
-	Email    string `gorm:"type:varchar(200);not null"`
-	Login    string `gorm:"type:varchar(200);not null;unique"`
-	Password string `gorm:"type:varchar(200);not null"`
-	Accepted bool   `gorm:"type:boolean;not null"`
+	ID         string `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Name       string `gorm:"type:varchar(200);not null"`
+	Email      string `gorm:"type:varchar(200);not null"`
+	Login      string `gorm:"type:varchar(200);not null;unique"`
+	Password   string `gorm:"type:varchar(200);not null"`
+	Accepted   bool   `gorm:"type:boolean;not null"`
+	Permission string `gorm:"type:jsonb;not null"`
 }
